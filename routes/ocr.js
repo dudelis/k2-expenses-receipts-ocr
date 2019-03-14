7f90b2b63908b8b44f301f133273066ece027787
dudelis <fukszon.kostya@gmail.com>
2019-03-14 15:15:57 -0700

    Add abbyy api and authentication checker

--- routes/ocr.js
+++ routes/ocr.js
@@ -3,6 +3,22 @@ var router = express.Router();
 
 var abbyyocr = require('../ocr/abbyyocr');
 
+
+//Check that authorization is Basic and provided.
+//Otherwise, we send the header for K2.
+router.use((req, res, next) =>{
+    const authType = (req.headers.authorization || '').split(' ')[0];
+    //Verify auth is Basic
+    if (authType != 'Basic') {
+        //Set specific Header for K2 Rest Broker
+        res.set('WWW-Authenticate', 'Basic realm="401"')
+        res.status(401).send('Authentication required.')
+        return
+    }
+    next();
+});
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     if (!req.headers.authorization){
